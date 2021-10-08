23a1e79ae05280af3817bd354a63a921f2ed6da1
Gideon Crawley <gscrawley@gmail.com>
2021-10-07 23:05:28 -0700

    navigation works

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Bands from './BandScreen'
-import Stats from './StatScreen'
+import Bands from './screens/BandScreen'
+import Stats from './screens/StatScreen'
 
 const Stack = createStackNavigator();
 
--- screens/BandScreen.js
+++ screens/BandScreen.js
@@ -1,10 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function Bands() {
+export default function Bands({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-    </View>
+      <Button
+          title="see Stats"
+          onPress={() => navigation.navigate('Stats')}
+        />   
+          </View>
   );
 }
